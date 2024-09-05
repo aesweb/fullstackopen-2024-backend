6344a45e9bc7e397498e629dd3c5991c5d028847
aesweb <aes.sncr@gmail.com>
2024-09-05 14:47:20 +0300

    exercises 3.19-3.21

--- models/person.js
+++ models/person.js
@@ -17,8 +17,31 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Name is required'],
+  },
+  number: {
+    type: String,
+    required: [true, 'Phone number is required'],
+    validate: [
+      {
+        validator: function (v) {
+          return /^(\d{2,3})-(\d+)$/.test(v) && v.length >= 8;
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number! It must be at least 8 characters long and in the format XX-XXXXXXX or XXX-XXXXXXX.`,
+      },
+      {
+        validator: function (v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number! It should only contain numbers and a single hyphen.`,
+      },
+    ],
+  },
 });
 
 personSchema.set('toJSON', {
